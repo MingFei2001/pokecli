f0683caa7db3f82a51d74e0cac5ac6279e10c78e
MingFei2001 <mingfei2001@gmail.com>
2025-05-27 10:22:37 +0800

    added basic code
    unproven to work yet

--- main.c
+++ main.c
@@ -1,4 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ASCII_DIR "ascii/" // set ascii art directory
+#define MAX_PATH 256       // set max buffer size for path
+
+void print_ascii(const char *filename) {
+  FILE *fp = fopen(filename, "r");
+  if (!fp) {
+    printf("could not open %s\n", filename);
+    return;
+  }
+
+  printf("\033[1;32m"); // set green color
+  char line[1024];
+  while (fgets(line, sizeof(line), fp)) {
+    printf("%s", line);
+  };
+  printf("\033[0m\n"); // reset color
+}
 
 int main() {
   printf("hello");
